0e7d0a45ece84995c2cff543412d2b787214855e
mo5e5 <malteoppermann@gmx.net>
2023-12-01 14:38:17 +0100

    ...

--- app/src/main/java/com/example/tschakkaduschaffstdas/ui/MainFragment.kt
+++ app/src/main/java/com/example/tschakkaduschaffstdas/ui/MainFragment.kt
@@ -5,22 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.tschakkaduschaffstdas.MainActivity
-import com.example.tschakkaduschaffstdas.R
 import com.example.tschakkaduschaffstdas.databinding.FragmentMainBinding
-import com.example.tschakkaduschaffstdas.ui.adapter.ItemAdapter
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
 
-    private lateinit var adapter: ItemAdapter
-    private lateinit var mainActivity: MainActivity
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
