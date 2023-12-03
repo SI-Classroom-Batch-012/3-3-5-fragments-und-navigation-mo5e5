2fb263899050fa6422d238655272e571dd824d87
mo5e5 <malteoppermann@gmx.net>
2023-12-03 19:11:59 +0100

    ...

--- app/src/main/java/com/example/tschakkaduschaffstdas/data/model/Info.kt
+++ app/src/main/java/com/example/tschakkaduschaffstdas/data/model/Info.kt
@@ -1,6 +1,6 @@
 package com.example.tschakkaduschaffstdas.data.model
 
 data class Info(
-    val headline: String,
-    val contentLine: String,
+    var headline: String,
+    var contentLine: String,
 )
--- app/src/main/java/com/example/tschakkaduschaffstdas/ui/DetailFragment.kt
+++ app/src/main/java/com/example/tschakkaduschaffstdas/ui/DetailFragment.kt
@@ -1,17 +1,22 @@
 package com.example.tschakkaduschaffstdas.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.fragment.navArgs
 import com.example.tschakkaduschaffstdas.MainActivity
+import com.example.tschakkaduschaffstdas.R
 import com.example.tschakkaduschaffstdas.databinding.FragmentDetailBinding
+import com.example.tschakkaduschaffstdas.ui.adapter.ItemAdapter
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private lateinit var adapter: ItemAdapter
     private val args: DetailFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +33,49 @@ class DetailFragment : Fragment() {
         val mainActivity = activity as MainActivity
         val content = mainActivity.dataset[position]
 
-
         binding.headlineMTV.text = content.headline
         binding.contentLineMTV.text = content.contentLine
 
+        adapter = ItemAdapter(mainActivity.dataset)
+
+        binding.editBTN.setOnClickListener {
+            addNoteDialog(position)
+        }
+
+    }
+
+    private fun addNoteDialog(position: Int) {
+
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        val inflater = requireActivity().layoutInflater
+        val dialogView = inflater.inflate(R.layout.add_note_dialog, null)
+
+        val headlineET = dialogView.findViewById<EditText>(R.id.headline_ET)
+        val contentET = dialogView.findViewById<EditText>(R.id.contentLine_ET)
+
+        val mainActivity = activity as MainActivity
+        val noteToEdit = mainActivity.dataset[position]
+
+        headlineET.setText(noteToEdit.headline)
+        contentET.setText(noteToEdit.contentLine)
+
+        dialogBuilder.setView(dialogView)
+            .setTitle("Edit Note")
+            .setPositiveButton("edit note") { _, _ ->
+                val textHeadlineInput = headlineET.text.toString()
+                val textContentLineInput = contentET.text.toString()
+
+                noteToEdit.headline = textHeadlineInput
+                noteToEdit.contentLine = textContentLineInput
+
+                adapter.newData(mainActivity.dataset)
+
+                binding.headlineMTV.text = textHeadlineInput
+                binding.contentLineMTV.text = textContentLineInput
+            }
+            .setNegativeButton("cancel") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .show()
     }
 }
